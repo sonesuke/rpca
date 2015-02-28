493c1515912b42703ad12b38eac531cd0bf72eee
sonesuke <iamsonesuke@gmail.com>
2015-03-01 00:28:38 +0900

    bug fix

--- rpca.py
+++ rpca.py
@@ -21,6 +21,14 @@ def l1norm(x):
     return l1norm
 
 
+def norm2Squared(x):
+    l1norm = 0.0
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            l1norm += x[i, j] * x[i, j]
+    return l1norm
+
+
 class RPCA:
 
     MAX_ITERS = 228
@@ -41,30 +49,27 @@ class RPCA:
 
 
     def computeRSVD(self):
-        mu = self.X.shape[1] * self.X.shape[0] / (4 * l1norm(X))
-        objPrev = 0.5 * np.power(np.linalg.norm(X), 2)
+        mu = self.X.shape[0] * self.X.shape[1] / (4 * l1norm(X))
+        objPrev = 0.5 * norm2Squared(X)
         obj = objPrev
         tol = 1e-8 * objPrev
         diff = 2 * tol
-        it = 0
 
+        it = 0
         while diff > tol and it < MAX_ITERS:
             nuclear_norm = self.computeS(mu)
             l1Norm = self.computeL(mu)
             l2Norm = self.computeE()
-
             obj = self.computeObjective(nuclear_norm, l1Norm, l2Norm)
             diff = np.abs(objPrev - obj)
             objPrev = obj
-
             mu = self.computeDynamicMu()
-
             it += 1
 
         return self.L, self.S, self.E
 
     def computeL(self, mu):
-        LPenalty = lpenalty * mu
+        LPenalty = self.lpenalty * mu
         u, d, v = np.linalg.svd(self.X - self.S, full_matrices=False)
         penalizedD = softThreshold1d(d, LPenalty)
         D = np.diag(penalizedD)
@@ -72,15 +77,14 @@ class RPCA:
         return np.sum(penalizedD) * LPenalty
 
     def computeS(self, mu):
-        SPenalty = spenalty * mu
+        SPenalty = self.spenalty * mu
         penalizedS = softThreshold2d(self.X - self.L, SPenalty)
         self.S = penalizedS
-        return l1norm(penalizedS,) * SPenalty
+        return l1norm(penalizedS) * SPenalty
 
     def computeE(self):
         self.E = self.X - self.L - self.S
-        norm = np.linalg.norm(self.E)
-        return np.power(norm, 2)
+        return norm2Squared(self.E)
 
     def computeObjective(self, nuclaernorm, l1norm, l2norm):
         return 0.5 * l2norm + nuclaernorm + l1norm
@@ -89,7 +93,7 @@ class RPCA:
         m = self.E.shape[0]
         n = self.E.shape[1]
 
-        E_sd = np.std(self.E)
+        E_sd = np.std(np.reshape(self.E, (m * n, 1)))
         mu = E_sd * np.sqrt(2 * np.max((m, n)))
         return np.max((0.01, mu))
 
@@ -109,8 +113,7 @@ lpenalty = 0.0
 spenalty = 0.0
 MAX_ITERS = 500
 
-X = np.zeros((ts.shape[0], 24), dtype=float)
-print X.shape
+X = np.zeros((14, 24), dtype=float)
 a = 0
 x = 0
 while a < ts.shape[0]:
@@ -121,20 +124,23 @@ while a < ts.shape[0]:
         a += 1
     x += 1
 
-# X = np.transpose(X)
+X = np.transpose(X)
 
 rpca = RPCA()
-L, S, E = rpca.calculate(X, 5.0, 3.5)
-
-print L
+#L, S, E = rpca.calculate(X, 1.0, 0.2857738033247)
+L, S, E = rpca.calculate(X, 1.0, 0.2)
 
 import matplotlib.pyplot as plt
 
-xp_s = np.transpose(L)[0, :]
+xp_s = np.reshape(np.transpose(L), (ts.shape[0], 1))
+ep_s = np.reshape(np.transpose(E), (ts.shape[0], 1))
+sp_s = np.reshape(np.transpose(S), (ts.shape[0], 1))
 
 
 print ts.shape
 print xp_s.shape
 plt.plot(ts)
 plt.plot(xp_s)
+plt.plot(ep_s)
+plt.plot(sp_s)
 plt.show()
